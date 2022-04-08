ff1e8d6ceefc0031ad340944bbb0a6f55cab44c5
GAbriel <gabrielsantos77239@gmail.com>
2022-04-08 16:00:17 -0300

    pedidos_atualizados

--- route/pedidos.js
+++ route/pedidos.js
@@ -34,39 +34,72 @@ router.get('/',(req,res,next) => {
 
 //insere um pedidos
 router.post('/',(req,res,next)=>{
+
     mysql.getConnection((error,conn)=>{
         if(error){return res.status(500).send({ error:error})}      
-        conn.query(
-            'INSERT INTO pedidos (id_produto,quantidade) VALUES (?,?);',
-            [req.body.id_produto, req.body.quantidade],
-            (error,result,field)=> {
-                conn.release();
-                if(error){return res.status(500).send({ error:error})}   
-                const response = {
-                    mensagem: 'pedido inserido com secesso',
-                    pedidoCriado: {
-                        id_pedido: result.id_pedidos,
-                        id_produto: req.body.id_produto,
-                        quantidade: req.body.quantidade,
-                        request:{
-                            tipo: 'GET',
-                            descricao:'insere um pedido',
-                            url:'http://localhost:3002/pedidos'
-                        }  
-                    }
-                }       
-                return res.status(201).send(response);
+        conn.query('SELECT * FROM produtos WHERE id_produto = ?;', [req.body.id_produto],
+        (error,result,field)=>{
+            if(error){return res.status(500).send({ error:error})}     
+            if (result.length == 0) {
+                return res.status(404).send({
+                message: 'produto não encontrado'
+                })
             }
-        )
-    });
+            conn.query(
+                'INSERT INTO pedidos (id_produto,quantidade) VALUES (?,?);',
+                [req.body.id_produto, req.body.quantidade],
+                (error,result,field)=> {
+                    conn.release();
+                    if(error){return res.status(500).send({ error:error})}   
+                    const response = {
+                        mensagem: 'pedido inserido com secesso',
+                        pedidoCriado: {
+                            id_pedido: result.id_pedidos,
+                            id_produto: req.body.id_produto,
+                            quantidade: req.body.quantidade,
+                            request:{
+                                tipo: 'GET',
+                                descricao:'insere um pedido',
+                                url:'http://localhost:3002/pedidos'
+                            }  
+                        }
+                    }       
+                    return res.status(201).send(response);
+                }
+            )
+        })
+    })
 });
 
 //retorna os dados de um pedidos
-router.get('/:id_pedido',(req,res,next)=>{
-    const id = req.params.id_pedidos;
-    res.status(200).send({
-        mensagem:'detalhe pedido',
-        id_pedido: id
+router.get('/:id_pedidos',(req,res,next)=>{
+    mysql.getConnection((error,conn)=> {
+        if(error){return res.status(500).send({ error:error})}
+        conn.query(
+            'SELECT * FROM pedidos WHERE id_pedidos = ?;',
+            [req.params.id_pedidos],
+            (error,result,fields)=>{
+                if(error){return res.status(500).send({ error:error})}
+                if (result.length == 0) {
+                    return res.status(404).send({
+                        message: 'Não foi encontrado pedido com este ID'
+                    })
+                }
+                const response = {
+                    pedidoCriado: {
+                        id_pedidos: result[0].id_pedidos,
+                        id_produto: result[0].id_produto,
+                        quantidade: result[0].quantidade,
+                        request:{
+                            tipo: 'GET',
+                            descricao:'retorna um pedidos',
+                            url:'http://localhost:3002/pedidos'
+                        }  
+                    }
+                }
+                return res.status(201).send(response)    
+            }
+        )        
     });    
 });
 
@@ -79,8 +112,28 @@ router.patch('/',(req,res,next)=>{
 
 //exclui um pedidos
 router.delete('/',(req,res,next)=>{
-    res.status(201).send({
-        mensagem:'pedido deletado'
+    mysql.getConnection((error,conn)=>{
+        if(error){return res.status(500).send({ error:error})}        
+        conn.query(
+            `DELETE FROM pedidos WHERE id_pedidos = ?`,[req.body.id_pedidos],                                          
+            (error,result,field)=> {
+                conn.release();
+                if(error){return res.status(500).send({ error:error})}    
+                const response ={
+                    mensagem : 'pedido removido',
+                    request:{
+                        tipo:'POST',
+                        descricao: 'insere um pedido',
+                        url:'http://localhost:3002/pedidos',
+                        body:{
+                            id_produto:'Number',
+                            quantidade:'Number'
+                        }
+                    }
+                }    
+                return res.status(202).send(response);
+            }
+        )
     });
 });
 
